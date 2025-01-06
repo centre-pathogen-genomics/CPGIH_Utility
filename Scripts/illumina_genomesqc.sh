95a279a28253751dfb2c04958f26df105af2773e
Calum Walsh <cazzlewazzle89@gmail.com>
2025-01-06 13:45:32 +1100

    Update illumina_genomesqc.sh

--- Scripts/illumina_genomesqc.sh
+++ Scripts/illumina_genomesqc.sh
@@ -55,7 +55,7 @@ do
 
 done < ${NAMES} >${OUTPUTDIR}/.temp_paths2
 
-paste ${NAMES}${OUTPUTDIR}/.temp_paths1 ${OUTPUTDIR}/.temp_paths2 > ${OUTPUTDIR}/.temp_manifest.tsv
+paste ${NAMES} ${OUTPUTDIR}/.temp_paths1 ${OUTPUTDIR}/.temp_paths2 > ${OUTPUTDIR}/.temp_manifest.tsv
 rm -f ${OUTPUTDIR}/.temp_paths1 ${OUTPUTDIR}/.temp_paths2 
 
 # ensure all specified input fastq files exist
