cdfe9f7168073502d22b4f2cdbb93bc07df1e631
Calum Walsh <cazzlewazzle89@gmail.com>
2025-02-27 15:15:27 +1100

    Update illumina_genomesqc.sh

--- Scripts/illumina_genomesqc.sh
+++ Scripts/illumina_genomesqc.sh
@@ -107,6 +107,10 @@ awk -F '\t' 'NR==FNR {exclude[$1]; next} !($1 in exclude)' ${OUTPUTDIR}/.emptysa
 
 # remove empty read sets from read stats file
 awk -F '\t' 'NR==FNR {exclude[$1]; next} !($1 in exclude)' ${OUTPUTDIR}/.emptysamples ${OUTPUTDIR}/.read_stats > ${OUTPUTDIR}/read_stats.tsv
+
+# print information about empty reads sets
+echo 'Removing the following samples from QC due to empty read sets"'
+cat ${OUTPUTDIR}/.emptysamples
     
 mkdir -p ${OUTPUTDIR}/KRAKEN/
 mkdir -p ${OUTPUTDIR}/SPADES/
