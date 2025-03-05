a32ca6cae99ae9d671c6cada455db570511d8f68
Calum Walsh <cazzlewazzle89@gmail.com>
2025-03-06 09:26:40 +1100

    Update ont_backup_stats_share.sh

--- Scripts/ont_backup_stats_share.sh
+++ Scripts/ont_backup_stats_share.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-# USAGE: /home/cwwalsh/Scripts/DAMG/ont_backup_stats_share.sh runID renamingTSV /optional/path/to/share_template.txt
+# USAGE: ont_backup_stats_share.sh runID renamingTSV /optional/path/to/share_template.txt
 
 set -e
 RUNID=$1
 RENAMINGTSV=$2
 SHARETEMPLATE=$3
 
-INPUTDIR=/home/damg/for_transfer/${RUNID}
-OUTPUTDIR=/home/damg/data/${RUNID}
+INPUTDIRECTORY=/home/damg/for_transfer/"$RUNID"
+OUTPUTDIRECTORY=/home/damg/data/"$RUNID"
 
 # CONFIRM THAT INPUT DIR EXISTS
-if [ ! -d ${INPUTDIR} ]
+if [ ! -d "$INPUTDIRECTORY" ]
 then
 
     echo "Input Directory Does Not Exist"
@@ -19,12 +19,12 @@ then
 
 else
 
-    echo "Input Directory:" ${INPUTDIR}
+    echo "Input Directory:" "$INPUTDIRECTORY"
 
 fi
 
 # CONFIRM THAT OUTPUT DIR DOES NOT EXIST
-if [ -d ${OUTPUTDIR} ]
+if [ -d "$OUTPUTDIRECTORY" ]
 then
 
     echo "Output Directory Already Exists"
@@ -33,13 +33,13 @@ then
 fi
 
 # CONFIRM THAT SHARING TEMPLATE FILE EXISTS IF PROVIDED
-if [ -z ${3} ]
+if [ -z "$3" ]
 then
     
     echo "Sharing File Template Not Specified. Will Not Generate Share Link"
 
 else
-    if [ ! -f ${SHARETEMPLATE} ]
+    if [ ! -f "$SHARETEMPLATE" ]
     then
 
         echo "Sharing File Template Does Not Exist"
@@ -50,7 +50,7 @@ else
 fi
 
 # CONFIRM INPUT FASTQ DIR EXISTS
-if [ ! -d ${INPUTDIR}/fastq_pass ] && [ ! -d ${INPUTDIR}/fastq ] && [ ! -d ${INPUTDIR}/fastq-pass ]
+if [ ! -d "$INPUTDIRECTORY"/fastq_pass ] && [ ! -d "$INPUTDIRECTORY"/fastq ] && [ ! -d "$INPUTDIRECTORY"/fastq-pass ]
 then
 
     echo "Input FASTQ Directory Does Not Exist"
@@ -59,7 +59,7 @@ then
 fi
 
 # CONFIRM INPUT POD5 DIR EXISTS
-if [ ! -d ${INPUTDIR}/pod5_pass ] && [ ! -d ${INPUTDIR}/pod5 ]
+if [ ! -d "$INPUTDIRECTORY"/pod5_pass ] && [ ! -d "$INPUTDIRECTORY"/pod5 ]
 then
 
     echo "Input POD5 Directory Does Not Exist"
@@ -68,7 +68,7 @@ then
 fi
 
 # CONFIRM INPUT REPORT DIR EXISTS
-if [ ! -d ${INPUTDIR}/reports ] && [ ! -d ${INPUTDIR}/other_reports ]
+if [ ! -d "$INPUTDIRECTORY"/reports ] && [ ! -d "$INPUTDIRECTORY"/other_reports ]
 then
 
     echo "Input Reports Directory Does Not Exist"
@@ -79,79 +79,79 @@ fi
 # IF WRITE PERMISSIONS ARE SET CORRECTNLY ON INPUT DIR
 # MAKE OUTPUT DIR
 # MOVE FASTQ, POD5, REPORT, AND ANALYSIS DIRS TO OUTPUT DIR
-if [ -w ${INPUTDIR} ]
+if [ -w "$INPUTDIRECTORY" ]
 then
 
-    echo "Ouput Directory:" ${OUTPUTDIR}
-    mkdir ${OUTPUTDIR}
+    echo "Ouput Directory:" "$OUTPUTDIRECTORY"
+    mkdir "$OUTPUTDIRECTORY"
     
-    if [ -d ${INPUTDIR}/fastq_pass ]
+    if [ -d "$INPUTDIRECTORY"/fastq_pass ]
     then
 
-        mv ${INPUTDIR}/fastq_pass ${OUTPUTDIR}
+        mv "$INPUTDIRECTORY"/fastq_pass "$OUTPUTDIRECTORY"
 
     fi
 
-    if [ -d ${INPUTDIR}/fastq ]
+    if [ -d "$INPUTDIRECTORY"/fastq ]
     then
 
-        mv ${INPUTDIR}/fastq ${OUTPUTDIR}
+        mv "$INPUTDIRECTORY"/fastq "$OUTPUTDIRECTORY"
 
     fi
 
-    if [ -d ${INPUTDIR}/fastq-pass ]
+    if [ -d "$INPUTDIRECTORY"/fastq-pass ]
     then
 
-        mv ${INPUTDIR}/fastq-pass ${OUTPUTDIR}
-
-    fi
-    
-    if [ -d ${INPUTDIR}/fast5_pass ]
-    then
-
-        mv ${INPUTDIR}/fast5_pass ${OUTPUTDIR}
-
-    fi
-
-    if [ -d ${INPUTDIR}/pod5_pass ]
-    then
-
-        mv ${INPUTDIR}/pod5_pass ${OUTPUTDIR}
-
-    fi
-
-    if [ -d ${INPUTDIR}/fast5 ]
-    then
-
-        mv ${INPUTDIR}/fast5 ${OUTPUTDIR}
+        mv "$INPUTDIRECTORY"/fastq-pass "$OUTPUTDIRECTORY"
 
     fi
     
-    if [ -d ${INPUTDIR}/pod5 ]
+    if [ -d "$INPUTDIRECTORY"/fast5_pass ]
     then
 
-        mv ${INPUTDIR}/pod5 ${OUTPUTDIR}
+        mv "$INPUTDIRECTORY"/fast5_pass "$OUTPUTDIRECTORY"
 
     fi
 
-    if [ -d ${INPUTDIR}/reports ]
+    if [ -d "$INPUTDIRECTORY"/pod5_pass ]
     then
 
-        mv ${INPUTDIR}/reports ${OUTPUTDIR}
+        mv "$INPUTDIRECTORY"/pod5_pass "$OUTPUTDIRECTORY"
 
     fi
 
-    if [ -d ${INPUTDIR}/other_reports ]
+    if [ -d "$INPUTDIRECTORY"/fast5 ]
     then
 
-        mv ${INPUTDIR}/other_reports ${OUTPUTDIR}
+        mv "$INPUTDIRECTORY"/fast5 "$OUTPUTDIRECTORY"
+
+    fi
+    
+    if [ -d "$INPUTDIRECTORY"/pod5 ]
+    then
+
+        mv "$INPUTDIRECTORY"/pod5 "$OUTPUTDIRECTORY"
 
     fi
 
-    if [ -d ${INPUTDIR}/analysis ]
+    if [ -d "$INPUTDIRECTORY"/reports ]
     then
 
-        mv ${INPUTDIR}/analysis ${OUTPUTDIR}
+        mv "$INPUTDIRECTORY"/reports "$OUTPUTDIRECTORY"
+
+    fi
+
+    if [ -d "$INPUTDIRECTORY"/other_reports ]
+    then
+
+        mv "$INPUTDIRECTORY"/other_reports "$OUTPUTDIRECTORY"
+
+    fi
+
+    if [ -d "$INPUTDIRECTORY"/analysis ]
+    then
+
+        mv "$INPUTDIRECTORY"/analysis "$OUTPUTDIRECTORY"
 
     fi
 else
@@ -162,18 +162,23 @@ else
 fi
 
 # COPY RENAMING INFO FILE TO OUTPUT DIR
-cp ${RENAMINGTSV} ${OUTPUTDIR}/renaming.tsv
+cp "$RENAMINGTSV" "$OUTPUTDIRECTORY"/renaming.tsv
 
 # SET WORKING DIR TO OUTPUT FASTQ OR FASTQ_PASS DIR 
-if [ -d ${OUTPUTDIR}/fastq_pass/ ]
+if [ -d "$OUTPUTDIRECTORY"/fastq_pass/ ]
 then
 
-    cd ${OUTPUTDIR}/fastq_pass/
+    cd "$OUTPUTDIRECTORY"/fastq_pass/
 
-elif [ -d ${OUTPUTDIR}/fastq/ ]
+elif [ -d "$OUTPUTDIRECTORY"/fastq/ ]
 then
 
-    cd ${OUTPUTDIR}/fastq/
+    cd "$OUTPUTDIRECTORY"/fastq/
+
+elif [ -d "$OUTPUTDIRECTORY"/fastq-pass/ ]
+then
+
+    cd "$OUTPUTDIRECTORY"/fastq-pass/
 
 fi
 
@@ -181,25 +186,25 @@ fi
 # RENAME
 # DELETE ORIGINAL BARCODE DIRECTORY
 SAMPLECOUNT=$(ls -d * | wc -l)
-echo ${SAMPLECOUNT} "Samples Found"
+echo "$SAMPLECOUNT" "Samples Found"
     
-while IFS= read -r i j || [[ -n "$i" ]]
+while IFS= read -r i j || [[ -n "$i" ]] 
 do
 
-    FILECOUNT=$(ls -1 ${i}/ | wc -l)
+    FILECOUNT=$(ls -1 "$i"/ | wc -l)
 
-    if [ ${FILECOUNT} == 1 ]
+    if [ $FILECOUNT = 1 ]
     then
 
-        echo "One FASTQ For Sample" ${i} "No Concatentation Needed. Just Renaming."
-        mv ${i}/*${i}*.fastq.gz ${j}.fastq.gz
-	rmdir ${i}
+        echo "One FASTQ For Sample" "$i" "No Concatentation Needed. Just Renaming."
+        mv "$i"/*"$i"*.fastq.gz "$j".fastq.gz
+	    rmdir "$i"
 
     else
 
-        echo "Concatenating" ${FILECOUNT} "FASTQ From" ${i} "And Renaming."
-        pigz -cd -p 10 ${i}/*.fastq.gz | pigz -p 10 > ${j}.fastq.gz
-	rm -rf ${i}
+        echo "Concatenating" "$FILECOUNT" "FASTQ From" "$i" "And Renaming."
+        pigz -cd -p 10 "$i"/*.fastq.gz | pigz -p 10 > "$j".fastq.gz
+	    rm -rf "$i"
 
     fi
 
@@ -207,16 +212,16 @@ done < ../renaming.tsv
 
 # CREATE SEQ STATS
 echo "Generating Read Length and Quality Statistics"
-seqkit stats -abT *.fastq.gz | \
+/home/cwwalsh/Software/seqkit stats -abT *.fastq.gz | \
     cut -f 1,4,5,13,14,15 | \
-        csvtk pretty -t > ../seqkit_stats.tsv
+        /home/cwwalsh/Software/csvtk pretty -t > ../seqkit_stats.tsv
 
 # SHARE ALL FILES FROM RUN IF SHARING TEMPLATE FILE PROVIDED
-if [ ! -z ${3} ]
+if [ ! -z "$3" ]
 then
 
     ls *.fastq.gz | sed 's/.fastq.gz$//' > names.tsv
-    mdu share --input_file names.tsv --source . -t ${SHARETEMPLATE}
+    mdu share --input_file names.tsv --source . -t "$SHARETEMPLATE"
     rm -f names.tsv
 
 fi
@@ -225,4 +230,4 @@ fi
 cat ../seqkit_stats.tsv
 
 # REMOVE EMPTY INPUT DIRECTORY
-rmdir ${INPUTDIR}
+rmdir "$INPUTDIRECTORY"
